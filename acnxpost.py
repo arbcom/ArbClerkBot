f79758a58f291c994ec84fba988884b3e17f28db
Bradv <bradv.wp@gmail.com>
2019-04-08 15:08:59 +0000

    Added crossposting to user talk pages.

--- acnxpost.py
+++ acnxpost.py
@@ -2,6 +2,7 @@ import mwclient
 import json
 import wikitextparser as parser
 import logging
+import re
 
 def run(wiki):
     ACN = u"Wikipedia:Arbitration Committee/Noticeboard"
@@ -11,16 +12,17 @@ def run(wiki):
 
     """
     load WP:ACN
-    ensure that the last editor is an arb or clerk (userlink on AUTH)
     for each section
     if section
         is level 2
         ends with a signature
         does not contain a link to WT:ACN
     then
+        ensure that the last editor of the page is an arb or clerk (userlink on AUTH)
         add Discuss this link
         create section on WT:ACN, if one by the same name does not already exist
         xpost to AN, if a section by the same name does not already exist
+        search for links to user pages, ensure they are within the current section, and xpost
 
     Any error (edit conflict, protected page, etc.) should result in the bot trying the edit again on the next run.
     To prevent duplicates, it errs on the side of not creating sections with duplicate names.
@@ -31,9 +33,23 @@ def run(wiki):
         def auth():
             lasteditor = wiki.revisions([page.revision])[0]['user']
             authpage = wiki.pages[AUTH]
-            authusers = [user.page_title for user in authpage.links(namespace=2)]
+            authusers.extend([user.page_title for user in authpage.links(namespace=2)])
             return lasteditor in authusers
-        
+
+        def xpost(target, title, announcement, ignoreerrors=False):
+            try:
+                logging.info('Crossposting to ' + target + '#' + title)
+                p = wiki.pages[target]
+                if p.text().find("== " + title + " ==") == -1:
+                    p.save(p.text() + '\n== ' + title + ' ==\n' + announcement, '/* ' + title + ' */ Crossposting from [[' + ACN + ']] (bot)', minor=False, bot=False)
+                else:
+                    logging.warning('Section already exists.')
+            except Exception as e:
+                if ignoreerrors:
+                    logging.error("Exception occurred", exc_info=True)
+                else:
+                    raise e
+                        
         page = wiki.pages[ACN]
         parsed = parser.parse(page.text())
         updated = False
@@ -54,15 +70,15 @@ def run(wiki):
                     else:
                         logging.warning('Section already exists.')
 
-                    xpost = [AN]
-                    xpost_content = "\n== " + title + " ==\n" + announcement
-                    for target in xpost:
-                        logging.info('Crossposting to ' + target + '#' + title)
-                        p = wiki.pages[target]
-                        if p.text().find("== " + title + " ==") == -1:
-                            p.save(p.text() + xpost_content, '/* ' + title + ' */ Crossposting from [[WP:ACN]] (bot)', minor=False, bot=False)
-                        else:
-                            logging.warning('Section already exists.')
+                    xpost(AN, title, announcement)
+
+                    xpostusers = []
+                    for ul in [user.page_title for user in page.links(namespace=2)]:
+                        if re.search('[:|]' + ul + '[]|}]', announcement) and ul not in authusers:
+                            xpostusers.append("User talk:" + ul)
+                    for user in xpostusers:
+                        xpost(user, title, announcement, True)
+
                     updated = True
                 else:
                     break
