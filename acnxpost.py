080b85d6a44cce344f61b83f08093d4d9f4e3a01
Bradv <bradv.wp@gmail.com>
2019-04-11 02:55:52 +0000

    Check for new revision before parsing

--- acnxpost.py
+++ acnxpost.py
@@ -4,7 +4,11 @@ import wikitextparser as parser
 import logging
 import re
 
+__lastrev = 0
+
 def run(wiki):
+    global __lastrev
+    
     ACN = u"Wikipedia:Arbitration Committee/Noticeboard"
     TACN = u"Wikipedia talk:Arbitration Committee/Noticeboard"
     AN = u"Wikipedia:Administrators' noticeboard"
@@ -51,43 +55,46 @@ def run(wiki):
                     raise e
                         
         page = wiki.pages[ACN]
-        parsed = parser.parse(page.text())
-        updated = False
-        for section in parsed.sections:
-            if section.level == 2 and section.contents.strip().endswith("(UTC)") and section.contents.find(TACN) == -1:
-                if auth():
-                    title = section.title.strip()
-                    logging.info('Found new section ' + ACN + '#' + title)
-                    a = "\n: Discuss this at: '''[[" + TACN + "#" + title + "]]'''{{subst:hes}}\n\n"
-                    announcement = section.contents.strip() + a
-                    section.contents = announcement
+        if page.revision != __lastrev:
+            __lastrev = page.revision
+            logging.info('Current revision: ' + str(__lastrev))
+            parsed = parser.parse(page.text())
+            updated = False
+            for section in parsed.sections:
+                if section.level == 2 and section.contents.strip().endswith("(UTC)") and section.contents.find(TACN) == -1:
+                    if auth():
+                        title = section.title.strip()
+                        logging.info('Found new section ' + ACN + '#' + title)
+                        a = "\n: Discuss this at: '''[[" + TACN + "#" + title + "]]'''{{subst:hes}}\n\n"
+                        announcement = section.contents.strip() + a
+                        section.contents = announcement
 
-                    logging.info('Creating talk section ' + TACN + '#' + title)
-                    talkpage = wiki.pages[TACN]
-                    a = "\n== " + title + " ==\n: [[" + ACN + "#" + title + "|'''Original announcement''']]{{subst:hes}}\n"
-                    if talkpage.text().find("== " + title + " ==") == -1:
-                        talkpage.save(talkpage.text() + a, '/* ' + title + ' */ Creating talk page section (bot)', minor=True, bot=True)
+                        logging.info('Creating talk section ' + TACN + '#' + title)
+                        talkpage = wiki.pages[TACN]
+                        a = "\n== " + title + " ==\n: [[" + ACN + "#" + title + "|'''Original announcement''']]{{subst:hes}}\n"
+                        if talkpage.text().find("== " + title + " ==") == -1:
+                            talkpage.save(talkpage.text() + a, '/* ' + title + ' */ Creating talk page section (bot)', minor=True, bot=True)
+                        else:
+                            logging.warning('Section already exists.')
+
+                        xpost(AN, title, announcement)
+
+                        xpostusers = []
+                        for ul in [user.page_title for user in page.links(namespace=2)]:
+                            if re.search('[:|]' + ul + '[]|}]', announcement) and ul not in authusers:
+                                xpostusers.append("User talk:" + ul)
+                        for user in xpostusers:
+                            xpost(user, title, announcement, True)
+
+                        updated = True
                     else:
-                        logging.warning('Section already exists.')
+                        break
 
-                    xpost(AN, title, announcement)
-
-                    xpostusers = []
-                    for ul in [user.page_title for user in page.links(namespace=2)]:
-                        if re.search('[:|]' + ul + '[]|}]', announcement) and ul not in authusers:
-                            xpostusers.append("User talk:" + ul)
-                    for user in xpostusers:
-                        xpost(user, title, announcement, True)
-
-                    updated = True
-                else:
-                    break
-
-        if updated and parsed != page.text():
-            t = str(parsed)
-            while t.find('\n\n\n') > -1:
-                t = t.replace('\n\n\n', '\n\n')
-            logging.info('Updating ' + ACN)
-            page.save(t, summary='Adding links to talk page sections (bot)', minor=True, bot=True)
+            if updated and parsed != page.text():
+                t = str(parsed)
+                while t.find('\n\n\n') > -1:
+                    t = t.replace('\n\n\n', '\n\n')
+                logging.info('Updating ' + ACN)
+                page.save(t, summary='Adding links to talk page sections (bot)', minor=True, bot=True)
     except Exception as e:
         logging.error("Exception occurred", exc_info=True)
