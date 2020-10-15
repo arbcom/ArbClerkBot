77972e22f54e294a9847ccb6e7040c92528dcfbe
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2020-10-15 19:17:24 -0400

    Prepare for toolforge (#3)
    * add cookie management, stop continuous run
    * add [[Special:BotPassword]] support
    * Update settings per files
    * Update version number for overall PR
    * Forgot save cookies
    * Forgot to pool connection
    * add best practice newline per cmts

--- clerkbot.py
+++ clerkbot.py
@@ -4,6 +4,8 @@ import json
 import time
 import acnxpost
 import logging
+import os, requests
+from http.cookiejar import MozillaCookieJar
 
 logging.basicConfig(filename='debug.log', format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 logging.info('=== RESTART ===')
@@ -12,10 +14,24 @@ with open('settings.json') as f:
     settings = json.load(f)
     logging.info('Settings loaded')
 
-wiki = mwclient.Site(settings['site'], path=settings['path'], clients_useragent=settings['ua'])
-wiki.login(settings['user'], settings['password'])
+cookies_file = settings['cookie_path']
+
+cookie_jar = MozillaCookieJar(cookies_file)
+if os.path.exists(cookies_file):
+    # Load cookies from file, including session cookies (expirydate=0)
+    cookie_jar.load(ignore_discard=True, ignore_expires=True)
+logging.info('We have %d cookies' % len(cookie_jar))
+
+connection = requests.Session()
+connection.cookies = cookie_jar  # Tell Requests session to use the cookiejar.
+
+wiki = mwclient.Site(settings['site'], path=settings['path'], clients_useragent=settings['ua'], pool=connection)
+if not wiki.logged_in:
+	wiki.login(settings['user'], settings['bot_password'])
 logging.info("Logged in to " + settings['site'] + " as user " + settings['user'])
 
-while True:
-    acnxpost.run(wiki)
-    time.sleep(60)
+# Save cookies to file, including session cookies (expirydate=0)
+logging.info(connection.cookies)
+cookie_jar.save(ignore_discard=True, ignore_expires=True)
+
+acnxpost.run(wiki)
